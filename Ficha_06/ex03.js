3e40f2601f9e84a00a06afddb90142254c89c57d
Evandro <9160377@esmad.ipp.pt>
2018-03-31 23:54:45 +0100

    exercicio incompleto(ultima alinea)

---
+++ Ficha_06/ex03.js
@@ -0,0 +1,77 @@
+let filmes = []
+
+class Filme {
+    constructor(titulo, ano, genero) {
+        this.titulo = titulo
+        this.ano = ano
+        this.genero = genero
+    }
+
+    get titulo() {
+        return this._titulo
+    }
+
+    set titulo(novoTitulo) {
+        this._titulo = novoTitulo
+    }
+
+    get ano() {
+        return this._ano
+    }
+
+    set ano(novoAno) {
+        this._ano = novoAno
+    }
+
+    get genero() {
+        return this._genero
+    }
+
+    set genero(novoGenero) {
+        this._genero = novoGenero
+    }
+}
+
+window.onload = function () {
+
+    let fFilmes = document.getElementById("myForm")
+    let fTitulo = document.getElementById("titulo")
+    let fAno = document.getElementById("data")
+    let fGenero = document.getElementById("generos")
+
+    fFilmes.addEventListener("submit", function (event) {
+
+        let filme = new Filme(fTitulo.value, fAno.value, fGenero.value)
+
+        filmes.push(filme)
+        console.log(filmes)
+
+        atualizarTabela()
+        
+        // Previne do form não ser submetido (isso faria com que a página fizesse um refresh)
+        event.preventDefault()
+
+    })
+
+}
+
+function atualizarTabela() {
+    tabela = document.getElementById("table")
+
+    let txt = ""
+
+    txt = "<tr><th>TÍTULO</th><th>ANO</th><th>GÉNERO</th></tr>"
+
+    for (let i = 0; i < filmes.length; i++) {
+        txt += "<tr><td scope='row'>" + filmes[i].titulo + "</td><td>" + filmes[i].ano + "</td><td>" + filmes[i].genero + "</td><td><i class='fa fa-trash-o'></i></td></tr>"
+
+    }
+
+    tabela.innerHTML = txt
+
+}
+
+
+
+
+
